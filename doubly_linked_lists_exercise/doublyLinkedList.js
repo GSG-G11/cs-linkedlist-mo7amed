2e5e7b3e7fdec1415664b60feeaaa626e4d814d1
mo7amedehab97 <gazacityface@gmail.com>
2022-03-05 01:07:09 +0200

    pop method

--- doubly_linked_lists_exercise/doublyLinkedList.js
+++ doubly_linked_lists_exercise/doublyLinkedList.js
@@ -31,14 +31,14 @@ DoublyLinkedList.prototype.push = function(val){
   }
     
     // increment the length
+
   this.length++;
 
   return this;
 }
+DoublyLinkedList.prototype.unshift = function (val) {
 
-DoublyLinkedList.prototype.unshift = function(val){
-    
-}
+};
 
 DoublyLinkedList.prototype.insert = function(index, val){
     
@@ -57,17 +57,32 @@ DoublyLinkedList.prototype.set = function(index, val){
     
 }
 
-DoublyLinkedList.prototype.pop = function(){
-    
+DoublyLinkedList.prototype.pop = function () {
+  // handle empty case 
+     if (!this.head) {
+    return undefined;
+     }
+  // first I saved the last item in temp to return it 
+  const temp = this.tail;
+
+  // then i removed the pointer to the prev item 
+  this.tail.prev = this.tail;
+
+  // then decrement lenght => deleteted the last item and return the deleted item 
+  this.length--;
+  
+  // return the deleted item 
+  return temp.val;
 }
 
 DoublyLinkedList.prototype.shift = function(){
     
 }
 
-DoublyLinkedList.prototype.remove = function(index){
-    
-}
+DoublyLinkedList.prototype.remove = function (index) {
+
+};
+
 
 DoublyLinkedList.prototype.reverse = function(){
     
